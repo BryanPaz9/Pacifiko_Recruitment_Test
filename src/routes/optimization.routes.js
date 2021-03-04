e2e54ee1c7c7a68cf9d368a57271af7eea4df0fd
Bryan Gerardo Ascuc de Paz <bascuc@nabenik.com>
2021-03-04 14:39:45 -0600

    Changing type request for third part.

--- src/routes/optimization.routes.js
+++ src/routes/optimization.routes.js
@@ -4,7 +4,7 @@ var express = require("express");
 
 const OptimizationController = require('../controllers/optimization.controller');
 var api = express.Router();
-api.post('/prime-number/:number',OptimizationController.isPrimeNumber);
-api.post('/consecutive-sum/:n',OptimizationController.consecutiveSum);
+api.get('/prime-number/:number',OptimizationController.isPrimeNumber);
+api.get('/consecutive-sum/:n',OptimizationController.consecutiveSum);
 
 module.exports = api;
